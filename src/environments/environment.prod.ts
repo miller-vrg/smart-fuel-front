b967706d113e1a4e5979244168c410b27a866f18
Miler Vargas <mvargas@atower.co>
2026-03-22 16:56:10 -0500

    Corrrecion entornos producion y preproduccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // Use relative path if using Nginx reverse proxy or full URL for domain
-  wsUrl: '/', // WebSocket URL relative to root
+  apiUrl: 'https://smart-fuel-back.onrender.com/api', // Use relative path if using Nginx reverse proxy or full URL for domain
+  wsUrl: 'https://smart-fuel-back.onrender.com', // WebSocket URL relative to root
   osrmUrl: 'https://router.project-osrm.org/route/v1/driving'
 };
